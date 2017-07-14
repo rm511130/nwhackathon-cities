6fefecc7dc5b3bbb8bcab721158e6979fc18d994
Ralph Meira <rmeira@pivotal.io>
2017-07-13 21:32:40 -0400

    Added xyz initials to make each deployment unique
    Added xyz initials to make each deployment unique

--- cf_simple_instructions.sh
+++ cf_simple_instructions.sh
@@ -15,32 +15,47 @@
 # You can run one Cities Demo Project per PCF Space. More than one will lead to naming collisions
 # You can test each step of the way by accessing the individual http addresses shown below in comments
 #
+# Please note that in a shared domain environment, developers have to avoid creating the same URIs for
+# their Apps and Services. For example, users assigned to ORG A and ORG B are capable of pushing the 
+# cities-hello app with the following URI https://cities-hello.apps.nwhackathon.com, so whoever 
+# executes the cf push command first gets to keep that URI.
+# In order to avoid clashes, we recommend that you add your initials to the App & Service Names in the
+# script below
 
-cf delete cities-hello -f -r
-cf delete cities-service -f -r
-cf delete cities-ui -f -r
-cf delete-service cities-service-db -f
-cf delete-service cities-ws -f
+# The comment above applies to the deletion of Apps and Services as well
+# 07-13-2017: I'm updating this script to use unique names where I'm adding a fictious developer's initials
+# to the names of Apps and Services. The initials will be "xyz", so you can easily find and replace them
+# with your own initials
+
+cf delete xyz-cities-hello -f -r
+cf delete xyz-cities-service -f -r
+cf delete xyz-cities-ui -f -r
+cf delete-service xyz-cities-service-db -f
+cf delete-service xyz-cities-ws -f
 
 cd /work/nwhackathon-cities
 gradle assemble
 
 cd /work/nwhackathon-cities/cities-hello
-cf push cities-hello -f manifest.yml
-# You can test http://cities-hello.apps.nwhackathon.com
+cf push xyz-cities-hello -f manifest.yml
+# please note that the file manifest.yml also contains the xyz initials in some of its lines
+# You can test http://xyz-cities-hello.apps.nwhackathon.com
 
-cf create-service p-mysql 100mb cities-service-db
+cf create-service p-mysql 100mb xyz-cities-service-db
 cd ../cities-service/
-cf push cities-service -f manifest.yml
+cf push xyz-cities-service -f manifest.yml
+# please note that the file manifest.yml also contains the xyz initials in some of its lines
 # http://cities-service.apps.nwhackathon.com/cities
 # http://cities-service.apps.nwhackathon.com/cities/search/nameContains/?q=DE&page=1&size=100
 
-cf create-user-provided-service cities-ws -p '{ "citiesuri": "http://cities-service.apps.nwhackathon.com" }'
-# cf create-user-provided-service cities-ws -p "citiesuri"
-# citiesuri> http://cities-service.apps.nwhackathon.com
+cf create-user-provided-service xyz-cities-ws -p '{ "citiesuri": "http://xyz-cities-service.apps.nwhackathon.com" }'
+# Alternative (interactive) syntax
+# cf create-user-provided-service xyz-cities-ws -p "xyz-citiesuri"
+# citiesuri> http://xyz-cities-service.apps.nwhackathon.com
 cf s
 
 cd ../cities-ui
 cf push -f manifest.yml
-# http://cities-ui.apps.nwhackathon.com
+# please note that the file manifest.yml also contains the xyz initials in some of its lines
+# http://xyz-cities-ui.apps.nwhackathon.com
 
