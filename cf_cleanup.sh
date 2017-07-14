3b30763b09ec5296561aff942d28056944aed2d0
Ralph Meira <rmeira@pivotal.io>
2017-07-13 21:19:33 -0400

    Update to explain need for unique naming
    Update to explain the need for unique App and Service names

--- cf_cleanup.sh
+++ cf_cleanup.sh
@@ -1,3 +1,10 @@
+# In order to avoid URI clashes, where several developers are trying 
+# to use, for example, the same https://cities-hello.apps.nwhackathon.com 
+# we recommend that you add your initials to the app-names and service-names
+# So, when it's time to clean-up and delete apps and services
+# please make sure the apps and services you are deleting 
+# actually match the apps and services you pushed & created
+
 cf delete cities-hello -f -r
 cf delete cities-service -f -r
 cf delete cities-ui -f -r
